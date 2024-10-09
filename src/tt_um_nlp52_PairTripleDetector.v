5835e1b9283a085217f53180dea358e6b480be49
nlp52 <132784612+nlp52@users.noreply.github.com>
2024-10-08 20:48:21 -0400

    Update tt_um_nlp52_PairTripleDetector.v

--- src/tt_um_nlp52_PairTripleDetector.v
+++ src/tt_um_nlp52_PairTripleDetector.v
@@ -7,8 +7,6 @@
 // PairTripleDetector_GL
 //========================================================================
 
-`include "../ece2300/hw/PairTripleDetector_GL.v"
-
 `ifndef TT_UM_NLP52_PAIRTRIPLEDETECTOR_V
 `define TT_UM_NLP52_PAIRTRIPLEDETECTOR_V
 
@@ -40,3 +38,34 @@ PairTripleDetector_GL dut
   wire _unused = &{ena, clk, rst_n, 1'b0};
 
 endmodule
+
+
+`endif /* PAIR_TRIPLE_DETECTOR_GL_V */
+
+`ifndef PAIR_TRIPLE_DETECTOR_GL_V
+`define PAIR_TRIPLE_DETECTOR_GL_V
+
+module PairTripleDetector_GL
+(
+  input  wire in0,
+  input  wire in1,
+  input  wire in2,
+  output wire out
+);
+
+  //''' ACTIVITY '''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+  // Implement pair/triple detector using explicit gate-level modeling
+  //>'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+  wire w, x, y;
+
+  or ( w,   in0, in1 );
+  and( x,   in0, in1 );
+  and( y,   w,   in2 );
+  or ( out, y,   x   );
+
+  //<'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+endmodule
+
+`endif /* PAIR_TRIPLE_DETECTOR_GL_V */
